599ba7e490f7db22474bfe32b58640e9efa7955f
Aswin <aswin.limenzy@gmail.com>
2024-09-04 17:55:54 +0530

    fix

--- src/Components/Effect.jsx
+++ src/Components/Effect.jsx
@@ -1,8 +1,10 @@
-import React, { useEffect, useState } from 'react'
+import React, { Fragment, useEffect, useState } from 'react'
 
 const Effect = () => {
 
-    const [counter, setCounter] = useState(0)
+    // const [counter, setCounter] = useState(0)
+
+    const [timer, setTimer] = useState(0)
 
     // useEffect( () => {
     //     console.log("Updating", counter)
@@ -11,23 +13,39 @@ const Effect = () => {
     //     }
     // }, [counter])
 
+    
 
     const handleMouseMove = (Event) => {
         console.log("X",Event.pageX, "Y",Event.pageY);
+        setAxis({x:Event.pageX, y:Event.pageY})
     }
+
+    const [axis, setAxis] = useState({x:0, y:0})
 
     useEffect ( () => {
         window.addEventListener("mousemove", handleMouseMove)
         return () => {
             window.removeEventListener("mousemove", handleMouseMove)
         }
+
+        // const interval = setInterval( () => {
+        //     setTimer((timer) => timer + 1)
+        // }, 1000);
+        // return () => {
+        //     clearInterval(interval)
+        // }
+
     }, [])
 
 
   return (
-    <div className='flex h-screen justify-center items-center'>
-        {/* <button onClick={() => setCounter(counter + 1)}>Click [{counter}]</button> */}
-    </div>
+    <Fragment>
+        <div className='w-10 h-10 bg-black blur-lg fixed' style={{top:'${axis.y}px',left:'${axis.x}px'}}></div>
+        <div className='flex h-screen justify-center items-center'>
+            {/* <button onClick={() => setCounter(counter + 1)}>Click [{counter}]</button> */}
+            {/* {timer} */}
+        </div>
+    </Fragment>
   )
 }
 
